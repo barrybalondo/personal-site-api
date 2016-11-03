6defaddc2a94a08003f0ec8f2a1caaa297bbfe04
barrybalondo <barry_balondo@hotmail.com>
2016-11-03 02:52:09 -0400

    edited routes that needs auth

--- controllers/postController.js
+++ controllers/postController.js
@@ -3,18 +3,15 @@ var auth = require('../middlewares/auth');
 
 module.exports = function(router, app) {
 
-  router.use(auth);
-  router.get('/', function(req, res) {
+
+  router.get('/',function(req, res) {
     res.json({ message: 'Welcome back Barry!' });   
   });
 
   // routes for /post/
   // ----------------------
-  router.route('/posts')
-
-    // put bear
-    .post(function(req, res){
-      var newPost = Posts({
+  router.post('/posts', auth, function(req, res){
+    var newPost = Posts({
       title: req.body.title,
       description: req.body.description
       });
@@ -23,10 +20,10 @@ module.exports = function(router, app) {
           throw err;
         res.send('Success');    
       });
-    })
- 
-    // get all post ascending
-    .get(function(req, res){
+  });
+  
+  
+  router.get('/posts', function(req, res){
       Posts.find({}).
       sort({ createdAt: -1 }).
       exec(function(err, posts){
@@ -34,14 +31,11 @@ module.exports = function(router, app) {
           throw err;
         res.json(posts);
       });  
-    });
+  });
 
   // routes for /post/:post_id
   // ----------------------
-  router.route('/posts/:id')
-
-    // get single post
-    .get(function(req, res){
+  router.get('/posts/:id', function(req, res){
       Posts.findById({ 
         _id: req.params.id 
       }, function(err, post){
@@ -49,10 +43,10 @@ module.exports = function(router, app) {
           throw err;
         res.json(post);
       })
-    })
+  });
 
-    // delete a post
-    .delete(function(req, res){
+  // delete a post
+  router.delete('/posts/:id', auth, function(req, res){
       Posts.remove({
         _id: req.params.id
       }, function(err,post){
@@ -60,7 +54,7 @@ module.exports = function(router, app) {
           throw err;
         res.json('Deleted!')
       })
-    });
+  });
 
   app.use('/api', router);
 
